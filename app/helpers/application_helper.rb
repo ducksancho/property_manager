eeedaccc1eabd7352f6da3e2655bb561b17ba7f4
Ducksan <daniel.cho.programmer@gmail.com>
2012-12-17 22:42:56 +1300

    add header design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def current_top_menu(menu, status)
+    # status(:active, :semi_active)
+    @current_top_menu = {menu => status}
+  end
+  def top_menu_tag(menu)
+    @links ||= {:dashboard => dashboard_path, :properties => dashboard_path}
+    case @current_top_menu.try(:[], menu)
+    when nil
+      link_to(t("top_menu.#{menu.to_s}"), @links[menu], :class => "deactive")
+    when :active
+      content_tag :div, t("top_menu.#{menu.to_s}"), :class => "active"
+    when :semi_active
+      link_to(t("top_menu.#{menu.to_s}"), @links[menu], :class => "semiActive")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 PropertyManager::Application.routes.draw do
   match '/design', :to => 'pages#design', :as => 'design'  
-  
+  match '/dashboard', :to => 'pages#dashboard', :as => 'dashboard'  
   root :to => "pages#design"
 end
